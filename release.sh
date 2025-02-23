77bc291172d72053103dbf09ef3f7b00ba5f7c61
blaufish <blaufish@users.noreply.github.com>
2025-02-23 18:06:37 +0100

    release.sh
    Just a stupid release tag script :)

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+version="0.0.1"
+tag=$(git tag -l -- "v$version")
+
+git fetch --tags
+
+if [ "$tag" != "" ]
+then
+    echo "Version $version allready exists."
+    exit 1
+fi
+
+./build.sh
+
+porcelain="$(git status --porcelain=v2)"
+
+if [ "$porcelain" ]
+then
+	echo "Directory is not clean. Exiting!"
+	exit 1
+fi
+
+git tag -a "v$version" -m "Release version $version"
