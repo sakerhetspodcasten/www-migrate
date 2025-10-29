0f27fd0ec43863789bcaaa9cd6730a7c70d8db4e
blaufish <blaufish@users.noreply.github.com>
2025-10-29 20:56:19 +0100

    download mp3 as necessary

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -1,14 +1,36 @@
 import argparse
+import hashlib
 import os.path
 import re
+import requests
+import tempfile
 import sys
 
-def download(url, filename):
+def download(url, download_dir):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        with open(filename, 'wb') as f:
+        with tempfile.TemporaryFile(dir=download_dir) as f:
+            m = hashlib.sha256()
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
+                m.update(chunk)
+
+            identifier = m.hexdigest()
+            filename = os.path.join(download_dir, identifier)
+
+            if os.path.exists(filename):
+                print(f'Warning, allready exists {filename}', file=sys.stderr)
+                return filename
+
+            f.seek(0)
+            with open(filename, 'wb') as f2:
+                while True:
+                    chunk = f.read(8192)
+                    if len(chunk) == 0:
+                        break
+                    f2.write(chunk)
+
+            return identifier
 
 def parse_md(fname, transcribe_header):
     if not os.path.isfile(fname):
@@ -34,14 +56,14 @@ def main():
             prog = 'transcribe.py',
             description = 'transcription tool', #TODO write a description
             epilog = 'Hope this help was helpful! :-)')
-    #parser.add_argument('--dir-mp3',
-    #        dest = 'dir_mp3',
-    #        required = True,
-    #        help = 'directory to download mp3s to')
+    parser.add_argument('--dir-mp3',
+            dest = 'dir_mp3',
+            required = True,
+            help = 'directory to download mp3s to')
     #parser.add_argument('--dir-trans',
     #        dest = 'dir_trans',
     #        required = True,
-    #        help = 'directory to download mp3s to')
+    #        help = 'directory to store transcriptions to')
     parser.add_argument('--transcribe-header',
             dest = 'transcribe_header',
             default = 'AI transkribering',
@@ -68,6 +90,7 @@ def main():
             continue
         print(f"{file}")
         print(f' * mp3: {mp3}')
+        downloaded_file = download(mp3, args.dir_mp3)
         #print(f' * transcribed: {transcribed}')
 
 if __name__ == "__main__":
